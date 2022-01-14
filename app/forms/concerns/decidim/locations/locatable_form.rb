7fd6f17cccaaeaeebae2208a4098e018ba030d24
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-01-14 13:04:14 +0200

    Add possibility to validate that all editable locations belong to the record

--- app/forms/concerns/decidim/locations/locatable_form.rb
+++ app/forms/concerns/decidim/locations/locatable_form.rb
@@ -9,6 +9,15 @@ module Decidim
         attribute :locations, Array[Decidim::Locations::LocationForm]
       end
 
+      class_methods do
+        attr_reader :locations_record_class
+
+        def validates_locations_for(record_class)
+          @locations_record_class = record_class
+          validate :validate_locations
+        end
+      end
+
       private
 
       def map_locations(model)
@@ -20,6 +29,24 @@ module Decidim
           else
             model.locations.map { |location| LocationForm.from_model(location) }
           end
+      end
+
+      def validate_locations
+        provided_ids = locations.map { |l| l.id.presence }.compact
+        if id.blank?
+          return unless locations.any? { |l| l.id.present? }
+
+          errors.add(:locations, I18n.t("decidim.locations.form.unexisting_locations", ids: provided_ids.join(", ")))
+          return
+        end
+
+        record = self.class.locations_record_class.find_by(id: id)
+        return unless record
+
+        unexisting_ids = provided_ids - record.locations.pluck(:id)
+        return if unexisting_ids.blank?
+
+        errors.add(:locations, I18n.t("decidim.locations.form.unexisting_locations", ids: unexisting_ids.join(", ")))
       end
     end
   end
