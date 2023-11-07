a6c9870d4fc3bfe754edf905cc5e451d570293e6
Joonas <joonas.aapro@mainiotech.fi>
2023-11-07 13:35:49 +0200

    Fix lint errors

--- app/packs/src/decidim/locations/map/integration/forms.js
+++ app/packs/src/decidim/locations/map/integration/forms.js
@@ -1,83 +1,6 @@
 import getDistanceBetweenPoints from "./avg_coordinates.js";
 import addMarkerField from "./add_marker_field.js";
-
-const initializeTabs = (wrapperEl) => {
-  const mapEl = wrapperEl.querySelector("[data-decidim-map]");
-  const ctrl = $(mapEl).data("map-controller");
-  const tabsEl = wrapperEl.querySelector('[data-map-container="#pick_model_locations_map"]');
-  const typeLocWrapperEl = wrapperEl.querySelector(".type-locations-wrapper");
-  const mainTabs = tabsEl.querySelectorAll(":scope > li");
-  const moreTabs = tabsEl.querySelector("[data-more-tabs]").querySelectorAll(":scope > li");
-  const transparentActions = ["clear-markers"];
-
-  let activeTab = null;
-
-  const setActiveTab = (action) => {
-    const wasActivated = activeTab === action;
-
-    if (wasActivated) {
-      activeTab = null;
-    } else {
-      mainTabs.forEach((li) => li.classList.remove("is-active"));
-      activeTab = action;
-    }
-
-    const activeLi = tabsEl.querySelector(`:scope > li[data-action="${action}"]`);
-    if (activeLi) {
-      if (wasActivated) {
-        activeLi.classList.remove("is-active");
-      } else {
-        activeLi.classList.add("is-active");
-      }
-    }
-  };
-
-  const handleActionTab = (action) => {
-    const wasActivated = activeTab === action;
-
-    if (!transparentActions.includes(action)) {
-      setActiveTab(action);
-
-      ctrl.disablePlaceMarkers();
-      typeLocWrapperEl.classList.add("hide");
-      if (wasActivated) {
-        return;
-      }
-    }
-
-    if (action === "add-markers") {
-      ctrl.enablePlaceMarkers();
-    } else if (action === "type-loc") {
-      typeLocWrapperEl.classList.remove("hide");
-    } else if (action === "clear-markers") {
-      ctrl.clearMarkers();
-    }
-  };
-
-  if (Object.keys(ctrl.markers).length === 0) {
-    handleActionTab("add-markers");
-  }
-
-  mainTabs.forEach((li) => {
-    const action = li.dataset.action;
-    if (action === "more") {
-      return;
-    }
-
-    li.querySelector(":scope > a").addEventListener("click", (ev) => {
-      ev.preventDefault();
-      handleActionTab(action);
-    });
-  });
-
-  moreTabs.forEach((li) => {
-    const action = li.dataset.action;
-    li.querySelector(":scope > a").addEventListener("click", (ev) => {
-      ev.preventDefault();
-      handleActionTab(action);
-    })
-  });
-};
+import initializeTabs from "./initialize_tabs.js";
 
 export default () => {
   const addInputGroup = (markerFieldContainer, addressData, wrapperEl) => {
@@ -268,7 +191,7 @@ export default () => {
           // When the `inputDiv`'s input fields have not been added yet, the reverse geocoding
           // is not completed yet, so the user cannot edit the address yet.
           return;
-        } else if (inputDiv && inputDiv.hasChildNodes()){
+        } else if (inputDiv && inputDiv.hasChildNodes()) {
           editModalEl.querySelector(".location-fields input[name=address]").value = inputDiv.querySelector(".location-address").value;
         } else {
           locFields.querySelector("[data-modal-address]").setAttribute("disabled", true);
