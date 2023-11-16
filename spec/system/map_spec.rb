a2e5ae8028bc445a227e690546edd4d1fc318c68
Joonas <joonas.aapro@mainiotech.fi>
2023-11-16 10:43:11 +0200

    Edit spec for github

--- spec/system/map_spec.rb
+++ spec/system/map_spec.rb
@@ -367,7 +367,7 @@ describe "Map", type: :system do
           find("#autoComplete_result_1").click
           click_button "Add"
           expect(page).to have_css(".leaflet-marker-draggable")
-          expect(find("#dummy_locations__index__address", visible: :all).value).to eq("Veneentekijäntie 6, Finland")
+          find_field("dummy_locations__index__address", type: :hidden, with: "Veneentekijäntie 6, Finland")
           expect(page).to have_css(".leaflet-marker-draggable", count: 1)
         end
       end
