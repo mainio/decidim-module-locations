32ad1991c92b41c777a7e79c57663b5fa72585f4
Joonas <joonas.aapro@mainiotech.fi>
2026-08-17 13:52:35 +0300

    Update Settings Helper Types

--- app/helpers/concerns/decidim/locations/settings_helper_extensions.rb
+++ app/helpers/concerns/decidim/locations/settings_helper_extensions.rb
@@ -14,9 +14,10 @@ module Decidim
           float: :number_field,
           text: :text_area,
           select: :select_field,
-          scope: :scope_field,
           enum: :collection_radio_buttons,
-          time: :datetime_field
+          time: :datetime_field,
+          integer_with_units: :integer_with_units,
+          taxonomy_filters: :taxonomy_filters
         }.freeze)
       end
     end
